f4bb3f532f322c7ba2a97bd3a67cec0c130bbd43
Yusuf <khanmohammadyusuf384@gmail.com>
2026-04-21 11:21:07 +0530

    Removed unused generateToken function from auth

--- backend/src/auth.js
+++ backend/src/auth.js
@@ -10,10 +10,6 @@ function hashPassword(password = "") {
   return crypto.createHash("sha256").update(String(password)).digest("hex");
 }
 
-function generateToken() {
-  return crypto.randomBytes(24).toString("hex");
-}
-
 function generateJWT(user) {
   return jwt.sign(
     { userId: user.id, email: user.email },
